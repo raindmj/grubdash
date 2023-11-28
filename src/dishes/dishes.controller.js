e58d78756151372493c56934efaeeefbf6971592
raindmj <lorrainedmj@gmail.com>
2023-11-28 12:11:06 -0600

    create method completed

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -11,6 +11,82 @@ function list(req, res, next) {
   res.send({ data: dishes });
 }
 
+function requiredField(prop) {
+  return function (req, res, next) {
+    if (req.body.data[prop]) {
+      next();
+    } else {
+      next({
+        status: 400,
+        message: `${prop} property is required.`,
+      });
+    }
+  };
+}
+
+function cannotBeEmptyString(prop) {
+  return function (req, res, next) {
+    if (req.body.data[prop].length > 0) {
+      next();
+    } else {
+      next({
+        status: 400,
+        message: `${prop} must not be an empty value.`,
+      });
+    }
+  };
+}
+
+function priceGreaterThanZero(req, res, next) {
+  const { price } = req.body.data;
+  if (price > 0) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: `price must be greater than 0.`,
+    });
+  }
+}
+
+function priceIsAnInteger(req, res, next) {
+  const { price } = req.body.data;
+  if (Number.isInteger(price)) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: `price must be an integer.`,
+    });
+  }
+}
+
+function create(req, res, next) {
+  const { name, description, price, image_url } = req.body.data;
+//   console.log("LOOK HERE********************", req.body.data)
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+//   console.log("DISH CREATED WITH NEW DATA*********", res.body)
+  res.status(201).send({ data: newDish });
+}
+
 module.exports = {
   list,
+  create: [
+    requiredField("name"),
+    requiredField("description"),
+    requiredField("image_url"),
+    cannotBeEmptyString("name"),
+    cannotBeEmptyString("description"),
+    cannotBeEmptyString("image_url"),
+    priceGreaterThanZero,
+    priceIsAnInteger,
+    create,
+  ],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -3,6 +3,6 @@ const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;

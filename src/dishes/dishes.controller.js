ee911b164d1a83bb529555791955c87cc8517b6c
raindmj <lorrainedmj@gmail.com>
2023-11-28 13:20:01 -0600

    read method completed

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,10 +7,13 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+//list
 function list(req, res, next) {
   res.send({ data: dishes });
 }
 
+//does property exist in request?
 function requiredField(prop) {
   return function (req, res, next) {
     if (req.body.data[prop]) {
@@ -18,12 +21,13 @@ function requiredField(prop) {
     } else {
       next({
         status: 400,
-        message: `${prop} property is required.`,
+        message: `${prop} property is required`,
       });
     }
   };
 }
 
+//does property contain an empty string?
 function cannotBeEmptyString(prop) {
   return function (req, res, next) {
     if (req.body.data[prop].length > 0) {
@@ -31,12 +35,13 @@ function cannotBeEmptyString(prop) {
     } else {
       next({
         status: 400,
-        message: `${prop} must not be an empty value.`,
+        message: `${prop} must not be an empty value`,
       });
     }
   };
 }
 
+//is price greater than 0?
 function priceGreaterThanZero(req, res, next) {
   const { price } = req.body.data;
   if (price > 0) {
@@ -44,11 +49,12 @@ function priceGreaterThanZero(req, res, next) {
   } else {
     next({
       status: 400,
-      message: `price must be greater than 0.`,
+      message: `price must be greater than 0`,
     });
   }
 }
 
+//is price an integer?
 function priceIsAnInteger(req, res, next) {
   const { price } = req.body.data;
   if (Number.isInteger(price)) {
@@ -56,14 +62,15 @@ function priceIsAnInteger(req, res, next) {
   } else {
     next({
       status: 400,
-      message: `price must be an integer.`,
+      message: `price must be an integer`,
     });
   }
 }
 
+//create
 function create(req, res, next) {
   const { name, description, price, image_url } = req.body.data;
-//   console.log("LOOK HERE********************", req.body.data)
+  //   console.log("LOOK HERE********************", req.body.data)
   const newDish = {
     id: nextId(),
     name,
@@ -72,8 +79,30 @@ function create(req, res, next) {
     image_url,
   };
   dishes.push(newDish);
-//   console.log("DISH CREATED WITH NEW DATA*********", res.body)
+  //   console.log("DISH CREATED WITH NEW DATA*********", res.body)
   res.status(201).send({ data: newDish });
+}
+
+//was the dish found?
+function doesDishExist(req, res, next) {
+  const { dishId } = req.params;
+  //   console.log("DISH ID HERE*********", dishId)
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  //   console.log("FOUND DISH HERE*********", foundDish)
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  } else {
+    next({
+      status: 404,
+      message: `dish id not found: ${dishId}`,
+    });
+  }
+}
+
+//read
+function read(req, res, next) {
+  res.send({ data: res.locals.dish });
 }
 
 module.exports = {
@@ -89,4 +118,5 @@ module.exports = {
     priceIsAnInteger,
     create,
   ],
+  read: [doesDishExist, read],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -3,6 +3,8 @@ const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/:dishId").get(controller.read).all(methodNotAllowed)
+
 router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;

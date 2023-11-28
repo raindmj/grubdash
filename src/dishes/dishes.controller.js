b3cfb8c57554abd78563268a09b340818071b84a
raindmj <lorrainedmj@gmail.com>
2023-11-28 11:14:37 -0600

    list route finished

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,10 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function list(req, res, next) {
+  res.send({ data: dishes });
+}
+
+module.exports = {
+  list,
+};
